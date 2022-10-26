80f2443ece2b5cc6ede01af9b2f0664e7c5ba16c
Nand Kumar <nand.kumar@bitcs.in>
2022-10-26 15:24:47 +0530

    nand commits undo merged changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
-
+import { Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        {/* <Route path="/login" element={<Login />} /> */}
+      </Routes>
     </div>
   );
 }
