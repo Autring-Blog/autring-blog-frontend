6af522c648a9f99dc68016b1597d8c6273fda960
Tariq Khan <Khantarikact@gmail.com>
2022-10-27 19:25:42 +0530

    add select card

--- src/components/card/card.component.js
+++ src/components/card/card.component.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import { useState } from 'react';
 
 import './card.styles.css';
 
 const Card = ({ data }) => {
+  const [select, setSelect] = useState(false);
+
+  const handleClick = () => {
+    setSelect(!select);
+  };
+
   return (
-    <div className='card'>
-      <div className='black-background'></div>
-      <div className='card-image'>
-        <img src={require(`../../assets/${data.img}`)} alt='img' />
+    <div className={`card`} onClick={handleClick}>
+      <div className={`${select ? 'select' : ''}`}>
+        <div className='black-background'></div>
+        <div className='card-image'>
+          <img src={require(`../../assets/${data.img}`)} alt='img' />
+        </div>
+        <p>{data.name}</p>
       </div>
-      <p>International</p>
     </div>
   );
 };
--- src/pages/favourite/favourite.component.js
+++ src/pages/favourite/favourite.component.js
@@ -4,42 +4,51 @@ import Card from '../../components/card/card.component';
 
 import './favourite.styles.css';
 
+const data = [
+  {
+    id: 1,
+    img: 'a3dacf52205c26dbf0690c8f55aa407e1.png',
+    name: 'National',
+  },
+  {
+    id: 2,
+    img: 'a3dacf52205c26dbf0690c8f55aa407e1.png',
+    name: 'National',
+  },
+  {
+    id: 3,
+    img: 'a3dacf52205c26dbf0690c8f55aa407e1.png',
+    name: 'National',
+  },
+  {
+    id: 4,
+    img: 'a3dacf52205c26dbf0690c8f55aa407e1.png',
+    name: 'National',
+  },
+  {
+    id: 5,
+    img: 'a3dacf52205c26dbf0690c8f55aa407e1.png',
+    name: 'National',
+  },
+  {
+    id: 6,
+    img: 'a3dacf52205c26dbf0690c8f55aa407e1.png',
+    name: 'National',
+  },
+];
+
 const Favourite = () => {
   const [input, setInput] = useState('');
+  const [dataList, setDataList] = useState([
+    ...data.map((item) => {
+      return { ...item, selectedItem: false };
+    }),
+  ]);
 
-  // const data = [1, 2, 3, 4, 5, 6];
-  const data = [
-    {
-      id: 1,
-      img: 'a3dacf52205c26dbf0690c8f55aa407e1.png',
-      name: 'National',
-    },
-    {
-      id: 2,
-      img: 'a3dacf52205c26dbf0690c8f55aa407e1.png',
-      name: 'National',
-    },
-    {
-      id: 3,
-      img: 'a3dacf52205c26dbf0690c8f55aa407e1.png',
-      name: 'National',
-    },
-    {
-      id: 4,
-      img: 'a3dacf52205c26dbf0690c8f55aa407e1.png',
-      name: 'National',
-    },
-    {
-      id: 5,
-      img: 'a3dacf52205c26dbf0690c8f55aa407e1.png',
-      name: 'National',
-    },
-    {
-      id: 6,
-      img: 'a3dacf52205c26dbf0690c8f55aa407e1.png',
-      name: 'National',
-    },
-  ];
+  const toggleSelect = (id) => {
+    dataList[id].selectedItem = !dataList[id].selectedItem;
+    setDataList(dataList);
+  };
 
   return (
     <div className='favourite'>
@@ -54,8 +63,10 @@ const Favourite = () => {
         />
       </div>
       <div className='card-list'>
-        {data.map((item, idx) => (
-          <Card data={item} key={idx} />
+        {dataList.map((item, idx) => (
+          <div key={idx} onClick={() => toggleSelect(idx)}>
+            <Card data={item} />
+          </div>
         ))}
       </div>
       <div className='btn btn-yellow'>Done</div>
