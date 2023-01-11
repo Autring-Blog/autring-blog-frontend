dd985808f1517c1c993ccfcc31f260a159b07f83
Tariq Khan <khantarikact@gmail.com>
2023-01-11 12:21:48 +0530

    tariq commit hamburger menu

--- src/components/Layout/Navbar/Navbar.jsx
+++ src/components/Layout/Navbar/Navbar.jsx
@@ -1,64 +1,69 @@
 import './Navbar.css';
 import NavbarImage from '../../../assets/images/autring-image.png';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 const navbarItems = [
   {
-    value: "international",
-    name: "International"
+    value: 'international',
+    name: 'International',
   },
   {
-    value: "national",
-    name: "National"
+    value: 'national',
+    name: 'National',
   },
   {
-    value: "politics",
-    name: "Politics"
+    value: 'politics',
+    name: 'Politics',
   },
   {
-    value: "defence",
-    name: "Defence"
+    value: 'defence',
+    name: 'Defence',
   },
   {
-    value: "science",
-    name: "Science and Technology"
+    value: 'science',
+    name: 'Science and Technology',
   },
   {
-    value: "sports",
-    name: "Sports"
+    value: 'sports',
+    name: 'Sports',
   },
   {
-    value: "education",
-    name: "Education"
+    value: 'education',
+    name: 'Education',
   },
   {
-    value: "others",
-    name: "Others"
+    value: 'others',
+    name: 'Others',
   },
-
-]
+];
 function Navbar() {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(!open);
+  };
+
   return (
     <div className='navbar'>
       <div className='brand-logo'>
         {<img src={NavbarImage} alt='brand-logo' />}
       </div>
       <div className='item-list'>
-        <Link to={'/'} className='item'>Home</Link>
-        {navbarItems && navbarItems.map((navItem) => (
-          <Link to={`/blogs/${navItem.value}`} className='item' key={navItem.value}>{navItem.name}</Link>
-        ))}
-        {/* <a className='item'>Home</a>
-        <a className='item'>International</a>
-        <a className='item'>National</a>
-        <a className='item'>Politics</a>
-        <a className='item'>Defence</a>
-        <a className='item'>Science and Technology</a>
-        <a className='item'>Sports</a>
-        <a className='item'>Education</a>
-        <a className='item'>Others</a> */}
+        <Link to={'/'} className='item'>
+          Home
+        </Link>
+        {navbarItems &&
+          navbarItems.map((navItem) => (
+            <Link
+              to={`/blogs/${navItem.value}`}
+              className='item'
+              key={navItem.value}
+            >
+              {navItem.name}
+            </Link>
+          ))}
       </div>
       <div className='navbar-search'>
-        {/* <i className='big search link icon'></i>{' '} */}
         <input
           className='search__input'
           type='text'
@@ -70,6 +75,47 @@ function Navbar() {
             search
           </i>
         </button>
+      </div>
+      {open && (
+        <>
+          <div className='hamburger-item-list'>
+            <Link to={'/'} className='item'>
+              Home
+            </Link>
+            {navbarItems &&
+              navbarItems.map((navItem) => (
+                <Link
+                  to={`/blogs/${navItem.value}`}
+                  className='item'
+                  key={navItem.value}
+                >
+                  {navItem.name}
+                </Link>
+              ))}
+          </div>
+          <div className='hamburger-navbar-search'>
+            <input
+              className='search__input'
+              type='text'
+              placeholder='Enter your search'
+              aria-label='search'
+            />
+            <button className='search__submit'>
+              <i
+                className='material-symbols-outlined'
+                aria-label='submit search'
+              >
+                search
+              </i>
+            </button>
+          </div>
+        </>
+      )}
+
+      <div className={`hamburger-menu ${open && 'open'} `} onClick={handleOpen}>
+        <span className='hamburger-top'></span>
+        <span className='hamburger-middle'></span>
+        <span className='hamburger-bottom'></span>
       </div>
     </div>
   );
