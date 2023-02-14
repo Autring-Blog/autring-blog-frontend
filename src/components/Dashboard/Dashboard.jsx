a3b64cbe773984cdaf7d018236a746d04d4732a2
Akhilesh S Pujar <akhileshpujar796@gmail.com>
2023-02-14 22:53:40 +0530

    dashboard-formik

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import BlogCard from "./card/BlogCard";
 import { logOut } from "../../redux/actions/userAction";
 import { useDispatch } from "react-redux";
+import{Formik ,Field,Form} from 'formik';
 
 import "./Dashboard.css";
 import Loader from "../Loader/Loader";
@@ -11,17 +12,31 @@ import { useNavigate } from "react-router-dom";
 const Dashboard = () => {
   const [loading, setLoading] = useState(false);
   const [blogList, setBlogList] = useState([]);
-  const [photo, setPhoto] = useState(null);
-  const [inPhotoTitle, setInPhotoTitle] = useState("");
-  const [category, setCategory] = useState("");
-  const [mainHeading, setMainHeading] = useState("");
-  const [shortDescription, setShortDescription] = useState("");
-  const [paragraphDescription, setParagraphDescription] = useState("");
+  const [chooseFile, setchooseFile] = useState(null);
+  const [selectCategory, setselectCategory] = useState("");
+  const [shortDescription, setshortDescription] = useState("");
+  const [description, setdescription] = useState("");
+  const [photoTitle, setphotoTitle] = useState("");
   const [isEdit, setIsEdit] = useState(false);
   const [itemId, setItemId] = useState("");
 
+
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
+  const options = [
+    { value: 'international', label: 'International' },
+    { value: 'national', label: 'National' },
+    { value: 'politics', label: 'Politics' },
+    { value:'defence',    label:'Defence'},
+    { value:'science and technology',label:'Science and Technology'},
+    { value:'sports',     label:'Sports'},
+    { value:'education',  label:'Education'},
+    { value:'others',     label:'Others'},
+  ];
+  
+
+  
 
   const getAllBlogs = async () => {
     setLoading(true);
@@ -47,58 +62,15 @@ const Dashboard = () => {
     getAllBlogs();
     setIsEdit(false);
   }, []);
+  
 
   const reset = () => {
-    setPhoto(null);
-    setInPhotoTitle("");
-    setCategory("");
-    setInPhotoTitle("");
-    setMainHeading("");
-    setShortDescription("");
-    setParagraphDescription("");
+    setchooseFile(null);
+    setselectCategory("");
+
   };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    console.log({
-      inPhotoTitle,
-      photo,
-      mainHeading,
-      shortDescription,
-      category,
-    });
-
-    try {
-      const postBlog = await axios.post(
-        "https://api.theautring.com/api/v1/postablog",
-        {
-          photo,
-          inPhotoTitle,
-          category,
-          mainHeading,
-          shortDescription,
-          paragraphDescription,
-        },
-        {
-          headers: {
-            "Content-Type": "multipart/form-data",
-          },
-          xhrFields: {
-            withCredentials: true,
-          },
-          withCredentials: true,
-        }
-      );
-
-      const res = await postBlog;
-      reset();
-      getAllBlogs();
-
-      console.log(res.data);
-    } catch (err) {
-      console.error("error post the blog", err);
-    }
-  };
+ 
 
   const deletePost = async (id) => {
     const confirm = window.confirm("Are you sure ?");
@@ -124,12 +96,11 @@ const Dashboard = () => {
     const editPost = blogList.filter((item) => id === item._id);
     const {
       _id,
-      category,
-      photo,
-      inPhotoTitle,
-      mainHeading,
+      selectCategory,
+      chooseFile,
+      photoTitle,
       shortDescription,
-      paragraphDescription,
+      description,
     } = editPost[0];
     setIsEdit(true);
     setItemId(_id);
@@ -138,17 +109,16 @@ const Dashboard = () => {
     console.log(editPost[0]);
     reset();
     const formData = new FormData();
-    formData.append("photo", photo);
+    formData.append("chooseFile", chooseFile);
     console.log(formData);
 
-    setPhoto(photo);
-    setInPhotoTitle(inPhotoTitle);
-    setCategory(category);
-    setInPhotoTitle(inPhotoTitle);
-    setMainHeading(mainHeading);
-    setShortDescription(shortDescription);
-    setParagraphDescription(paragraphDescription);
+    setchooseFile(chooseFile);
+    setphotoTitle(photoTitle);
+    setselectCategory(selectCategory);
+     setshortDescription(shortDescription);
+    setdescription(description);
     return editPost[0];
+    
   };
 
   const editPost = async () => {
@@ -158,11 +128,11 @@ const Dashboard = () => {
       const updateBlog = await axios.patch(
         `https://api.theautring.com/api/v1/updateblog/${id}`,
         {
-          category,
-          inPhotoTitle,
-          mainHeading,
+          selectCategory,
+           photoTitle,
+           chooseFile,
           shortDescription,
-          paragraphDescription,
+          description,
         },
         {
           xhrFields: {
@@ -171,9 +141,9 @@ const Dashboard = () => {
           withCredentials: true,
         }
       );
-      const updatePhoto = await axios.patch(
-        `https://api.theautring.com/api/v1/updateblogphoto/${id}`,
-        { photo },
+      const updatechooseFile = await axios.patch(
+        `https://api.theautring.com/api/v1/updateblogchooseFile/${id}`,
+        { chooseFile },
         {
           headers: {
             "Content-Type": "multipart/form-data",
@@ -185,12 +155,12 @@ const Dashboard = () => {
         }
       );
       const res = await updateBlog;
-      const changePhoto = await updatePhoto;
+      const changechooseFile = await updatechooseFile;
       getAllBlogs();
       reset();
       setIsEdit(false);
       console.log(res);
-      console.log(changePhoto);
+      console.log(changechooseFile);
     } catch (error) {
       console.error("Something went wrong", error);
     }
@@ -202,99 +172,101 @@ const Dashboard = () => {
 
   return (
     <>
-      <button className="btn-logout" onClick={handleLogOut}>
-        logout
-      </button>
+
       <div className="dashboard">
-        <div className="left-dashboard">
-          <form onSubmit={handleSubmit}>
-            <div className="form-control">
-              <input
-                type="file"
-                name="photo"
-                onChange={(e) => setPhoto(e.target.files[0])}
-                required
-              />
-            </div>
-            <div className="form-control">
-              <input
-                type="text"
-                id="photoTitle"
-                value={inPhotoTitle}
-                onChange={(e) => setInPhotoTitle(e.target.value)}
-                placeholder="Photo Title"
-              />
-            </div>
-            <div className="form-control">
-              {/* <input
-								type="text"
-								id="category"
-								value={category}
-								onChange={(e) => setCategory(e.target.value)}
-								placeholder="Category"
-								required
-							/> */}
-              <select
-                required
-                id="category"
-                value={category}
-                onChange={(e) => setCategory(e.target.value)}
-              >
-                <option selected disabled>
-                  Category
-                </option>
-                <option value="international">International</option>
-                <option value="national">National</option>
-                <option value="politics">Politics</option>
-                <option value="defence">Defence</option>
-                <option value="science and technology">
-                  Science and Technology
-                </option>
-                <option value="sports">Sports</option>
-                <option value="education">Education</option>
-                <option value="others">Others</option>
-              </select>
-            </div>
-            <div className="form-control">
-              <input
-                type="text"
-                id="mainHeading"
-                value={mainHeading}
-                onChange={(e) => setMainHeading(e.target.value)}
-                placeholder="Main Heading"
-                required
-              />
-            </div>
-            <div className="form-control">
-              <input
-                type="text"
-                id="shortDescription"
-                value={shortDescription}
-                onChange={(e) => setShortDescription(e.target.value)}
-                placeholder="Short Description"
-                required
-              />
-            </div>
+         <div className="left-dashboard">
+            <button className="btn-logout" onClick={handleLogOut}>
+             logout
+            </button>
+           <Formik
+             initialValues={{
+             chooseFile:'',
+             chooseFileTitle: '',
+             shortDescription: '',
+             description: '',
+             selectCategory:''
+            }}
+            onSubmit={(values,{setSubmitting}) => {
+            const handleSubmit = async (e) => {
+            e.preventDefault();
+           console.log({
+           chooseFile,
+           photoTitle,
+           selectCategory,
+           shortDescription,
+           description,
+            });
+
+            try {
+           const postBlog = await axios.post(
+           "https://api.theautring.com/api/v1/postablog",
+           {
+            chooseFile,
+            photoTitle,
+            selectCategory,
+            shortDescription,
+            description,
+           },
+           {
+            headers: {
+            "Content-Type": "multipart/form-data",
+            },
+           xhrFields: {
+            withCredentials: true,
+            },
+             withCredentials: true,
+           }
+       );
+
+             const res = await postBlog;
+            reset();
+            getAllBlogs();
+
+            console.log(res.data);
+          } catch (err) {
+            console.error("error post the blog", err);
+    }
+  };
+
+          }}
+    >
+         {({ isSubmitting }) => (
+           <Form onSubmit={Formik.handleSubmit}>
+           <Field name ="chooseFile"type="file" placeholder=" chooseFile" className="form-control" />
+
+           <Field name ="selectCategory" as="select" className="form-control">
+            <option value="">Select category</option>
+
+            {options.map(option=>(
+              <option key={option.value} value={option.value}>{option.label}</option>
+            ))}
+
+            </Field>
+
+            <Field name ="chooseFileTitle"type="text" placeholder="chooseFile title" className="form-control"/>
+
+            <Field name="shortDescription" placeholder="shortDescription" type="text"className="form-control"/>
+
             <div className="form-control">
               <textarea
                 type="text"
                 id="description"
-                value={paragraphDescription}
-                onChange={(e) => setParagraphDescription(e.target.value)}
                 placeholder="Description"
                 rows="10"
                 cols="30"
               ></textarea>
             </div>
-
+           </Form>
+           )}
+           
+         </Formik>
             {!isEdit ? (
-              <button>Add</button>
+              <button type ="submit" >Add</button>
             ) : (
               <button type="button" onClick={editPost}>
                 Edit
               </button>
             )}
-          </form>
         </div>
         <div className="right-dashboard">
           <div className="blog-list">
