beeb24aff696d139dfed9d0aa532a138646cc1bf
ibraham-vicdigit <96982180+ibrahim1165@users.noreply.github.com>
2023-02-16 23:18:52 +0600

    pix the dashboard

--- src/components/Auth/Signin/Signin.jsx
+++ src/components/Auth/Signin/Signin.jsx
@@ -28,6 +28,7 @@ const Signin = () => {
     }
     if (isAuthenticated) {
       navigate("/dashboard");
+    window.location.reload(true);
     }
   }, [dispatch, error, alert, isAuthenticated, navigate]);
 
