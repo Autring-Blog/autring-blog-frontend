8d12225fc30c40ed9a4b7d355995dca93eea3c54
SUBID DAS <itsmesubid@gmail.com>
2023-02-17 12:07:04 +0530

    Code `cleanup`

--- src/components/Auth/ProtectedRoute/ProtectedRoute.jsx
+++ src/components/Auth/ProtectedRoute/ProtectedRoute.jsx
@@ -1,34 +1,13 @@
-import React, { useEffect } from 'react';
-import { Navigate, Outlet, useLocation } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import Loader from '../../Loader/Loader';
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 
-const ProtectedRoute = ({children}) => {
-  const { loading, isAuthenticated } = useSelector((state) => state.user);
-  // if (loading === false && !isAuthenticated) {
-  //     return <Navigate to="/signin" />
-  // }
-  const location = useLocation()
-  const user = JSON.parse(localStorage.getItem('user'));
+const ProtectedRoute = () => {
+  const location = useLocation();
+  const user = JSON.parse(localStorage.getItem("user"));
 
   if (!user) {
     return <Navigate to="/signin" state={{ from: location }} />;
   }
-    return <Outlet />;
-
-
-
-
-  // if (!isAuthenticated) {
-  //   return <Navigate to='/signin' />;
-  // }
-
-
-  // if (user && user.role === 'admin') {
-  //   return <Navigate to='/dashboard' />;
-  // }
-  // return loading === false && <Outlet />;
+  return <Outlet />;
 };
 
 export default ProtectedRoute;
-
