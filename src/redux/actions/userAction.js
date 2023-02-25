cd9d78363cd178f2a3037c7db5dd78b9c0bbe5eb
sunil380875 <sunil380mishra@gmail.com>
2023-02-25 20:17:42 +0530

    logout redirect to home

--- src/redux/actions/userAction.js
+++ src/redux/actions/userAction.js
@@ -70,8 +70,7 @@ export const logOut = async () => {
   localStorage.clear();
   // deleteAllCookies();
 
-  window.location.reload(true);
-  // window.location.replace("http://www.theautring.com");
+  window.location.replace("http://www.theautring.com");
 };
 
 // // Load User
